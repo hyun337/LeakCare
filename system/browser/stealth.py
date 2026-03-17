42d0c889dfec4bf488a57ae2c26f4fa51ccd34ac
hyun337 <129953025+hyun337@users.noreply.github.com>
2026-03-17 22:18:46 +0900

    Update stealth.py

--- system/browser/stealth.py
+++ system/browser/stealth.py
@@ -1,1 +1,7 @@
-
+async def stealth_async(page):
+    await page.add_init_script("""
+        Object.defineProperty(navigator, 'webdriver', {get: () => undefined});
+        window.chrome = {runtime: {}};
+        Object.defineProperty(navigator, 'plugins', {get: () => [1, 2, 3]});
+        Object.defineProperty(navigator, 'languages', {get: () => ['ko-KR', 'ko']});
+    """)
