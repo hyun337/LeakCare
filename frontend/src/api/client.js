6159299bc5693dd9324e0d77d323c25a5a07b27b
najikkka <127591271+najikkka@users.noreply.github.com>
2026-05-16 22:14:57 +0900

    Update client.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,3 +1,5 @@
-const BASE_URL = 'https://d60e-121-67-233-19.ngrok-free.app/api/v1';
-
+const BASE_URL = 'https://0cff-2406-5900-1018-ac01-1850-404e-c095-7ca.ngrok-free.app/api/v1';
 export default BASE_URL;
+export const COMMON_HEADERS = {
+  'ngrok-skip-browser-warning': 'true',
+};
